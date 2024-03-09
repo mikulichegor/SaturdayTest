08462ea93e58d0d2cccc0d86535f31440525c9d6
egormikulich <mikulichegor@gmail.com>
2024-03-09 12:34:55 +0300

    Создал функцию меню

---
+++ main.py
@@ -0,0 +1,27 @@
+def menu():
+
+    answer = input('Выберите действие:\n+ - сложение\n- - вычитание\n* - умножение\n'
+                   '/ - деление\n** - возведение в степень\n% - остаток от деления\n корень - корень\n'
+                   'sin - синус\ncos - косинус\ntg - тангенс')
+    if answer == '+':
+        pass
+    elif answer == '-':
+        pass
+    elif answer == '*':
+        pass
+    elif answer == '/':
+        pass
+    elif answer == '**':
+        pass
+    elif answer == '%':
+        pass
+    elif answer == 'корень':
+        pass
+    elif answer == 'sin':
+        pass
+    elif answer == 'cos':
+        pass
+    elif answer == 'tg':
+        pass
+
+menu()
